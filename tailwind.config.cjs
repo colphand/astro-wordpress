a1c5406892d9dd417f8270f03f709bdd416e58c7
colphand <37404645+colphand@users.noreply.github.com>
2024-06-07 17:56:47 -0400

    com

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,29 +18,41 @@ module.exports = {
                   700: '#037947',
                   800: '#056039',
                   900: '#054F31',
+                  950: '#022C1C',
                 },
             },
+            boxShadow: {
+                'hard': '4px 4px 0px 0px black', 
+            }
         },
     },
     plugins: [
-        // require('daisyui'),
+        require('daisyui'),
         require('@tailwindcss/typography'),
     ],
-    // daisyui: {
-    //     themes: [
-    //         {
-    //             mytheme: {
-    //                 primary: '#d94948',
-    //                 secondary: '#4959ed',
-    //                 accent: '#38077c',
-    //                 neutral: '#1F2228',
-    //                 'base-100': '#1f2937',
-    //                 info: '#5eead4',
-    //                 success: '#126E52',
-    //                 warning: '#C49B08',
-    //                 error: '#FA1E29',
-    //             },
-    //         },
-    //     ],
-    // },
+    daisyui: {
+        themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: "dark", // name of one of the included themes for dark mode
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+        themeRoot: ":root", // The element that receives theme color CSS variables
+        // themes: [
+        //     {
+        //         mytheme: {
+        //             primary: '#d94948',
+        //             secondary: '#4959ed',
+        //             accent: '#38077c',
+        //             neutral: '#1F2228',
+        //             'base-100': '#1f2937',
+        //             info: '#5eead4',
+        //             success: '#126E52',
+        //             warning: '#C49B08',
+        //             error: '#FA1E29',
+        //         },
+        //     },
+        // ],
+    },
 };
