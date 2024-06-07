607c7ab0a1de257dd44875375eae3cac48d5b790
colphand <37404645+colphand@users.noreply.github.com>
2024-06-07 15:02:52 -0400

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,24 +2,30 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+        fontFamily: {
+            'display': ['Degular Display', 'ui-sans-serif', 'system-ui']
+        },
         extend: {},
     },
-    plugins: [require('daisyui')],
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    primary: '#d94948',
-                    secondary: '#4959ed',
-                    accent: '#38077c',
-                    neutral: '#1F2228',
-                    'base-100': '#1f2937',
-                    info: '#5eead4',
-                    success: '#126E52',
-                    warning: '#C49B08',
-                    error: '#FA1E29',
-                },
-            },
-        ],
-    },
+    plugins: [
+        // require('daisyui'),
+        require('@tailwindcss/typography'),
+    ],
+    // daisyui: {
+    //     themes: [
+    //         {
+    //             mytheme: {
+    //                 primary: '#d94948',
+    //                 secondary: '#4959ed',
+    //                 accent: '#38077c',
+    //                 neutral: '#1F2228',
+    //                 'base-100': '#1f2937',
+    //                 info: '#5eead4',
+    //                 success: '#126E52',
+    //                 warning: '#C49B08',
+    //                 error: '#FA1E29',
+    //             },
+    //         },
+    //     ],
+    // },
 };
