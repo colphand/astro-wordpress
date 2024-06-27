4dc55c27b72a67467d0694c48e058ffb080aa31b
colphand <37404645+colphand@users.noreply.github.com>
2024-06-27 16:48:32 -0400

    Commit before trying netlify again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -31,28 +31,13 @@ module.exports = {
         require('@tailwindcss/typography'),
     ],
     daisyui: {
-        themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "dark", // name of one of the included themes for dark mode
-        base: true, // applies background color and foreground color for root element by default
-        styled: true, // include daisyUI colors and design decisions for all components
-        utils: true, // adds responsive and modifier utility classes
-        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-        themeRoot: ":root", // The element that receives theme color CSS variables
-        // themes: [
-        //     {
-        //         mytheme: {
-        //             primary: '#d94948',
-        //             secondary: '#4959ed',
-        //             accent: '#38077c',
-        //             neutral: '#1F2228',
-        //             'base-100': '#1f2937',
-        //             info: '#5eead4',
-        //             success: '#126E52',
-        //             warning: '#C49B08',
-        //             error: '#FA1E29',
-        //         },
-        //     },
-        // ],
-    },
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
